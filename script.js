c74a98281da8392f070384f3c7b144db2b46f347
POURMHERE <103439512+pourmhere@users.noreply.github.com>
2024-08-13 07:37:33 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Create an object called student with a property called name
+const student = {
+    name: "John"
+};
+
+// Add a property to the Object prototype called getKeys()
+Object.prototype.getKeys = function() {
+    // Use Object.keys to get an array of keys in the object
+    return Object.keys(this);
+};
+
+// Example usage
+console.log(student.getKeys()); // Output: ['name']
+
